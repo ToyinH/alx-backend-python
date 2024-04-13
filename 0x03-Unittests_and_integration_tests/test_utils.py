2d4797c994f9a2ed82fb5651ea04ad268dd0ebcd
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-04-13 03:47:44 +0100

    update

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -24,6 +24,18 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
+    @parameterized.expand([
+        ({}, ("a",), "Key 'a' not found in nested map"),
+        ({"a": 1}, ("a", "b"), "Key 'b' not found in nested map")
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_exception_message):
+        """
+        Test method to test that KeyError is raised for invalid access.
+        """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), expected_exception_message)
+
    
 if __name__ == "__main__":
     unittest.main()
